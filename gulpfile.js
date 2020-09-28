d9a11219ee0aaf38902a29b3b5275ed3cadab1e4
AlexLeo <leontev.mmaker@gmail.com>
2020-09-28 16:44:52 +0300

    Project was initialized

--- gulpfile.js
+++ gulpfile.js
@@ -4,12 +4,12 @@ const gulp = require('gulp');
 const sass = require('gulp-sass');
 sass.compiler = require('node-sass');
 
-gulp.task('sass', function () {
+gulp.task('sass', function() {
     return gulp.src('src/scss/**/*.scss')
         .pipe(sass().on('error', sass.logError))
         .pipe(gulp.dest('src/css'))
 });
 
-gulp.task('watch', gulp.parallel(['sass']), () => {
-    gulp.watch('sass/**/*.scss', ['sass']);
+gulp.task('watch', function() {
+    gulp.watch('src/scss/**/*.scss', gulp.series(['sass']));
 });
